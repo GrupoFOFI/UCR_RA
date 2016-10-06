fbd045d2c82d09553a14b2affeade4a2b18bd0e1
Enrico Barbieri <ebarbieric@symbiotic.net>
2016-10-06 10:08:24 -0600

    Paquete de BL renombrado

--- Business Logic/src/main/java/com/enrico/businesslogic/test.java
+++
@@ -1,7 +0,0 @@
-package com.enrico.businesslogic;
-
-/**
- * Created by tete on 10/3/16.
- */
-public class test {
-}
---
+++ Business Logic/src/main/java/ra/inge/ucr/bl/MapsService.java
@@ -0,0 +1,9 @@
+package ra.inge.ucr.bl;
+
+/**
+ * Created by enrico on 10/6/16.
+ */
+
+public class MapsService {
+    
+}
